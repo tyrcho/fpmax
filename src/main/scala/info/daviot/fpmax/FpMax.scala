c6e29c83c509d03f2a1c10c7f310a662f091d6ff
Michel Daviot <michel@colisweb.com>
2018-08-10 22:13:15 +0200

    Single Abstract Method for Random

--- src/main/scala/info/daviot/fpmax/FpMax.scala
+++ src/main/scala/info/daviot/fpmax/FpMax.scala
@@ -4,9 +4,7 @@ import scala.util.Try
 
 import info.daviot.fpmax.StdLib._
 object FpMax extends App {
-  implicit val randomIO: Random[IO] = new Random[IO] {
-    override def nextInt(max: Int): IO[Int] = IO(() => util.Random.nextInt(max))
-  }
+  implicit val randomIO: Random[IO] = max => IO(() => util.Random.nextInt(max))
 
   implicit val consoleIO: Console[IO] = new Console[IO] {
     override def ps(s: String): IO[Unit] = IO(() => println(s))
@@ -70,50 +68,4 @@ object MyApp {
 
   private def inputNumber[F[_]: Program: Console]: F[Option[Int]] =
     readString.map(s => Try(s.toInt).toOption)
-}
-
-object StdLib {
-  case class IO[A](unsafeRun: () => A) { self =>
-    def map[B](f: A => B): IO[B]         = IO(() => f(self.unsafeRun()))
-    def flatMap[B](f: A => IO[B]): IO[B] = IO(() => f(self.unsafeRun()).unsafeRun())
-  }
-  object IO {
-    def point[A](a: => A) = IO(() => a)
-  }
-
-  //a Monad
-  trait Program[F[_]] {
-    def finish[A](a: => A): F[A]
-    def chain[A, B](fa: F[A], f: A => F[B]): F[B]
-    def map[A, B](fa: F[A], f: A => B): F[B]
-  }
-  object Program {
-    def apply[F[_]](implicit p: Program[F]): Program[F] = p
-  }
-  implicit class ProgramSyntax[F[_], A](fa: F[A]) {
-    def map[B](f: A => B)(implicit p: Program[F]): F[B]        = p.map(fa, f)
-    def flatMap[B](f: A => F[B])(implicit p: Program[F]): F[B] = p.chain(fa, f)
-  }
-  def point[F[_], A](a: => A)(implicit p: Program[F]): F[A] = p.finish(a)
-
-  trait Console[F[_]] {
-    def ps(s: String): F[Unit]
-    def rs: F[String]
-  }
-  object Console {
-    def apply[F[_]](implicit c: Console[F]): Console[F] = c
-  }
-
-  def printString[F[_]: Console](s: String): F[Unit] = Console[F].ps(s)
-  def readString[F[_]: Console]: F[String]           = Console[F].rs
-
-  trait Random[F[_]] {
-    def nextInt(max: Int): F[Int]
-  }
-  object Random {
-    def apply[F[_]](implicit r: Random[F]): Random[F] = r
-  }
-
-  def nextInt[F[_]: Random](max: Int): F[Int] = Random[F].nextInt(max)
-
 }

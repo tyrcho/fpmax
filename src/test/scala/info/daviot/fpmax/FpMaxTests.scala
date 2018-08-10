7cf7da3e16dcc1fb769fe1bedeeaa74bc9e392f9
Michel Daviot <michel@colisweb.com>
2018-08-10 18:29:10 +0200

    import initial code

--- src/test/scala/info/daviot/fpmax/FpMaxTests.scala
+++ src/test/scala/info/daviot/fpmax/FpMaxTests.scala
@@ -3,7 +3,7 @@ package info.daviot.fpmax
 import org.scalatest._
 
 class FpMaxTests extends FlatSpec with Matchers {
-  it should "start with a failing test" in {
-    true shouldBe false
+  it should "start with a ok test" in {
+    true shouldBe true
   }
 }
